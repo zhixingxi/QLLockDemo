423d188d38de3d2f04113697746ef72156796347
MQL-IT <mengqingling@MQLing.local>
2017-05-23 22:56:40 +0800

    创建工程

---
+++ LockLearnDemo/LockLearnDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LockLearnDemo
+//
+//  Created by MQL-IT on 2017/5/23.
+//  Copyright © 2017年 MQL-IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
